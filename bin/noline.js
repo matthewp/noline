306f9ff9632a1c95fc06e70726d9c94319c629c3
Matthew Phillips <matthew@matthewphillips.info>
2015-02-24 09:39:33 -0500

    Everything is working

--- bin/noline.js
+++ bin/noline.js
@@ -1,20 +1,29 @@
-#!/bin/env node
+#!/usr/bin/env node
 var fs = require('fs');
 var noline = require('../lib/noline');
 var denodeify = require('pdenodeify');
 var read = denodeify(fs.readFile);
 var write = denodeify(fs.writeFile);
+var argv = require('yargs')
+  .usage('Remove trailing newline characters from a file.')
+  .example('$0 foo.js', 'Remove trailing newline characters from foo.js')
+  .required(1, 'Must provide a file to remove trailing newlines for')
+  .version(require('../package.json').version, 'version')
+  .argv;
 
-var fileName = 'foo.js';
+var files = argv._;
+files.map(removeAndSave);
 
-read(fileName, 'utf8').then(convert).then(save).then(null, warn);
+function removeAndSave(fileName) {
+  return read(fileName, 'utf8').then(convert).then(save).then(null, warn);
+
+  function save(data) {
+    return write(fileName, data, 'utf8');
+  }
+}
 
 function convert(data) {
   return noline(data);
-}
-
-function save(data) {
-  return write(fileName, data, 'utf8');
 }
 
 function warn(err) {
